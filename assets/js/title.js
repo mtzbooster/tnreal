956c279cc99d6d4d9f1938a417c36f89feb5f9aa
mtzbooster <mtzbooster@hotmail.com>
2018-03-07 04:13:16 -0300

    update title

---
+++ assets/js/title.js
@@ -0,0 +1,6 @@
+	var scrl = " TNREAL OFICIAL ";
+	function scrlsts() {
+	 scrl = scrl.substring(1, scrl.length) + scrl.substring(0, 1);
+	 document.title = scrl;
+	 setTimeout("scrlsts()", 300);
+	}
